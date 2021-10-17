44e43dcc0488dec19af8e6318ad0b81d716a88ce
tom <2962703+Scutterman@users.noreply.github.com>
2021-10-17 11:55:25 +0100

    Add rules for newlines at end of file, single quotes, no dangling commas, and auto-semicolon insertion

--- gcp/functions/.eslintrc.js
+++ gcp/functions/.eslintrc.js
@@ -2,30 +2,40 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'] // Specify it only for TypeScript files
+      }
+    }
+  ],
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    sourceType: 'module'
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    '/lib/**/*' // Ignore built files.
   ],
   plugins: [
-    "@typescript-eslint",
-    "import",
+    '@typescript-eslint',
+    'import'
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-  },
-};
+    'quotes': ['error', 'single'],
+    'import/no-unresolved': 0,
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'eol-last': ['error', 'always']
+  }
+}
