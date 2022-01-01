25cfa4a8b5a067cadf082373c0746e7c9658c2d5
tom <2962703+Scutterman@users.noreply.github.com>
2022-01-01 11:49:27 +0000

    Store token id in fragment state, recover it in onCreate, use it when returning to the fragment

--- app/src/main/java/uk/co/cgfindies/youtubevogthumbnailcreator/AuthFragment.kt
+++ app/src/main/java/uk/co/cgfindies/youtubevogthumbnailcreator/AuthFragment.kt
@@ -11,6 +11,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -18,7 +21,9 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 
-class AuthFragment : Fragment() {
+class AuthFragment : Fragment(), DefaultLifecycleObserver {
+    private var tokenId: String? = null
+
     private fun onAuthCompleted() {
         val data = requireActivity().findViewById<EditText>(R.id.auth_token).text.toString()
         try {
@@ -33,11 +38,53 @@ class AuthFragment : Fragment() {
         requireActivity().findViewById<View>(R.id.not_a_token).visibility = VISIBLE
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super<Fragment>.onCreate(savedInstanceState)
+        tokenId = savedInstanceState?.getString("tokenId")
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        // app moved to foreground
+        if (tokenId == null) {
+            return
+        }
+        val url = getString(R.string.auth_api_url)
+        val authRequest = JsonObjectRequest(Request.Method.GET, "$url/getStoredToken?tokenId=$tokenId", null,
+            { response ->
+                val data = response?.toString() ?: return@JsonObjectRequest
+                try {
+                    val auth = Json.decodeFromString<AccessTokenResponse>(data)
+                    Utility.setAuthentication(auth, requireContext())
+                    tokenId = null
+                } catch (_error: Exception) {
+                    showError()
+                }
+            },
+            { error ->
+                Log.e("FETCH_AUTH_URL", "Could not get auth url", error)
+                Utility.showMessage(requireActivity(), R.string.fragment_auth_no_auth_url)
+            }
+        )
+
+        val queue = Volley.newRequestQueue(requireContext())
+        queue.add(authRequest)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        // app moved to background, no action required
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("tokenId", tokenId)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -49,7 +96,8 @@ class AuthFragment : Fragment() {
         val authRequest = JsonObjectRequest(Request.Method.GET, "$url/generateAuthUrl", null,
             { response ->
                 val authUrl = response?.getString("url")
-                if (authUrl == null) {
+                tokenId = response?.getString("tokenId")
+                if (authUrl == null || tokenId == null) {
                     Log.e("FETCH_AUTH_URL", "Auth URL Response was not in the correct format ${ response?.toString() ?: "NULL" }")
                     Utility.showMessage(requireActivity(), R.string.fragment_auth_no_auth_url)
                 } else {
